ea2299fc0ac88f3150fbe3a29cf0693767768581
juniorvilas <log_junior@hotmail.com>
2022-07-19 21:03:45 -0300

    page jsx (componente)

---
+++ src/pages/jsx.jsx
@@ -0,0 +1,18 @@
+import Layout from '../components/Layout'
+
+export default function JSX() {
+    return (
+        <Layout title="Entendendo JSX" >
+            <h1>
+                Introduzindo JSX
+            </h1>
+            <p>
+                É chamada JSX e é uma extensão de sintaxe para JavaScript.
+                Recomendamos usar JSX com o React para descrever como a UI deveria parecer.
+                JSX pode lembrar uma linguagem de template, mas que vem com todo o poder do JavaScript.
+                JSX produz “elementos” do React. Nós iremos explorar a renderização para o DOM na próxima seção.
+                Abaixo você descobrirá o básico de JSX necessário para começar.
+            </p>
+        </Layout>
+    )
+}
