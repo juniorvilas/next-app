42a9c053913378b3b2d31910a7235dad604c0756
juniorvilas <log_junior@hotmail.com>
2022-07-19 21:10:25 -0300

    page principal com acesso as outras pages

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,20 @@
-import Link from 'next/link';
+import Navegador from "../components/Navegador";
 
 export default function Home() {
   return(
-    <div>
-      <Link href="/estiloso">
-        Ir para Estiloso
-      </Link>
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      flexWrap: 'wrap',
+    }}>
+      <Navegador title="Estiloso" destino="/estiloso" />
+      <Navegador title="Exemple" destino="/exemple" color="blueviolet"/>
+      <Navegador title="JSX" destino="/jsx" color="crimson"/>
+      <Navegador title="Navegação 01" destino="/navegacao" color="green"/>
+      <Navegador title="Navegação Dinâmica" destino="/cliente/sp/123" color="blue"/>
+      <Navegador title="Componente com Estado" destino="/estado" color="pink"/>
     </div>
   )
 }
